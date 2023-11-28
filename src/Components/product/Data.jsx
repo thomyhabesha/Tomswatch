9f55d8cd8c3e7609a84db63d2132660189d6c102
thomyhabesha <thomastafese3337@gmail.com>
2023-11-28 17:02:52 +0300

    mkbghgl

--- src/Components/product/Data.jsx
+++ src/Components/product/Data.jsx
@@ -1,9 +1,9 @@
 import img1 from '../images/buywatch/BEXSONN-removebg-preview.png'
 import img2 from '../images/buywatch/hangingwatch1-removebg-preview.png'
-import img3 from '../images/buywatch/Clooney_wears_an_OMEGA_De_Ville_Prestige_in_his_award-winning_performance-removebg-preview.png'
-import img4 from '../images/buywatch/Introducing_Patek_Philippe_Calatrava__Clous_de_Paris__6119_Hand-Wound-removebg-preview.png'
-import img5 from '../images/buywatch/Lianfudai_watches_for_men_smart_watch_Men_Watch_Stainless_Steel_Top_Quailty_Luxury_Push_Button_Hidden_Clasp_Waterproof_Luminous_Date_Week_Sport_Wrist_Watches_-_836_BK_BK_S-removebg-preview.png'
-import img6 from '../images/buywatch/The_Rolex_Cellini_Collections__Here_s_What_You_Need_to_Know-removebg-preview.png'
+import img3 from '../images/buywatch/Clooney_wears.png'
+import img4 from '../images/buywatch/Introducing_Patek.png'
+import img5 from '../images/buywatch/goldenwatch.png'
+import img6 from '../images/buywatch/The_Rolex_Cellini.png'
 
 const Data=[
     
